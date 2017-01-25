6e867ac20029afd1ca30e3c71bdec35017e6201b
rastasheep <rastasheep3@gmail.com>
2017-01-25 18:18:41 +0100

    Remove gulp and all corresponding packages

--- src/client/index.spec.js
+++ src/client/index.spec.js
@@ -1,8 +1,9 @@
+/* global require */
 import './index';
 
 import 'angular-mocks';
 import 'bardjs/bard';
 
 // require all modules ending in ".spec.js"
-var testsContext = require.context(".", true, /.spec.js$/);
+var testsContext = require.context('.', true, /.spec.js$/);
 testsContext.keys().forEach(testsContext);
