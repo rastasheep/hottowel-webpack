8b6bbb63d866aa312a8971fc2338c3ac2c0b1b58
rastasheep <rastasheep3@gmail.com>
2017-02-17 22:05:40 +0100

    Add angular-loading-bar

--- src/client/app/core/core.module.js
+++ src/client/app/core/core.module.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import angularAnimate from 'angular-animate';
+import angularLoadingBar from 'angular-loading-bar';
 import angularSanitize from 'angular-sanitize';
 import appConfig from '../config';
 import blocksException from '../blocks/exception';
@@ -8,7 +9,7 @@ import blocksRouter from '../blocks/router';
 
 angular
   .module('app.core', [
-    angularAnimate, angularSanitize,
+    angularAnimate, angularSanitize, angularLoadingBar,
     blocksException, blocksLogger, blocksRouter,
     appConfig,
   ]);
