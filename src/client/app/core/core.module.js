0771db8cf3c75c16e042cda15109df5b50910978
rastasheep <rastasheep3@gmail.com>
2017-02-18 21:55:16 +0100

    Fix stylelint config

--- src/client/app/core/core.module.js
+++ src/client/app/core/core.module.js
@@ -12,5 +12,5 @@ angular
   .module('app.core', [
     angularAnimate, angularSanitize, angularLoadingBar,
     blocksException, blocksLogger, blocksRouter,
-    appConfig,
+    appConfig, appConstants,
   ]);
